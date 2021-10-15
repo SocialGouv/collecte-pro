1e1e3fa53942bb2b052fae69a9913c1202b647a0
Mindiell <github@mindiell.net>
2021-10-15 15:10:20 +0200

    fix: Amélioration de la gestion des droits

--- ecc/auth.py
+++ ecc/auth.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import Group
 from mozilla_django_oidc.auth import OIDCAuthenticationBackend
 from user_profiles.models import UserProfile
 
@@ -14,21 +15,27 @@ class EccOIDCAuthenticationBackend(OIDCAuthenticationBackend):
 
     def user_ecollecte(self, user, claims):
         """
-        Updating users from keycloak informations.
+        Mise à jour des utilisateurs depuis les informations KeyCloak.
         """
         user.first_name = claims.get("given_name", "")
         user.last_name = claims.get("family_name", "")
+        # Gestion des droits administrateurs
+        if "superuser" in claims.get("roles", []):
+            user.is_superuser = True
+            user.is_staff = True
+        else:
+            user.is_superuser = False
+            user.is_staff = False
         if "admin" in claims.get("roles", []):
             user.is_staff = True
-            user.is_superuser = True
-        elif "admin" not in claims.get("roles", []):
-            user.is_staff = False
-            user.is_superuser = False
+            user.groups.add(Group.objects.filter(name="admin_metier").first())
+        else:
+            if not user.is_superuser:
+                user.is_staff = False
+            user.groups.remove(Group.objects.filter(name="admin_metier").first())
         user.save()
-
-        if not user.is_staff:
-            if UserProfile.INSPECTOR in claims.get("roles", []):
-                user.profile.profile_type = UserProfile.INSPECTOR
-            else:
-                user.profile.profile_type = UserProfile.AUDITED
-            user.profile.save()
+        # Gestion des droits métier
+        user.profile.profile_type = UserProfile.AUDITED
+        if UserProfile.INSPECTOR in claims.get("roles", []):
+            user.profile.profile_type = UserProfile.INSPECTOR
+        user.profile.save()
