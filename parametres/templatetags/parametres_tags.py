af3eaeb42b67fd19c800b3812b1dded24bd4d677
Mindiell <github@mindiell.net>
2021-10-25 09:49:39 +0200

    #82 - Remise en place de la Marianne par défaut

--- parametres/templatetags/parametres_tags.py
+++ parametres/templatetags/parametres_tags.py
@@ -6,12 +6,18 @@ register = template.Library()
 
 @register.simple_tag
 def get_liens_footer_items():
-    return Parametre.objects.filter(code="LIEN_FOOTER").order_by('ordre')
+    return Parametre.objects.filter(code="LIEN_FOOTER").order_by("ordre")
 
 @register.simple_tag
 def get_logo_footer_item():
-    return Parametre.objects.filter(code="LOGO_FOOTER").first()
+    return Parametre.objects.filter(code="LOGO_FOOTER").first() or {
+        "url": "img/logo-footer.png",
+        "title": "Logo Marianne",
+    }
 
 @register.simple_tag
 def get_entity_picture_item():
-    return Parametre.objects.filter(code="ENTITY_PICTURE").first()
+    return Parametre.objects.filter(code="ENTITY_PICTURE").first() or {
+        "url": "img/picture-Republique-francaise.png",
+        "title": "Logo Marianne",
+    }
