0c958f20eab1a35e80d17031f3812ab09144f56a
almorin <alexandre.morin@kleegroup.com>
2022-12-23 15:01:09 +0100

    #149 - Le demandeur supprimé d'un espace de dépôt doit recevoir une notification
    - la suppression est faite avant l'envoi de mail

--- user_profiles/api_views.py
+++ user_profiles/api_views.py
@@ -33,11 +33,11 @@ class UserProfileViewSet(
         serializer = RemoveControlSerializer(data=request.data)
         if serializer.is_valid():
             control_id = serializer.data['control']
-            Access.objects.filter(Q(control=control_id) & Q(userprofile=profile)).first().delete()
             control = Control.objects.get(pk=control_id)
             user_api_post_remove.send(
                 sender=UserProfile, session_user=self.request.user, user_profile=profile,
                 control=control)
+            Access.objects.filter(Q(control=control_id) & Q(userprofile=profile)).first().delete()
             return Response({'status': f"Removed control {control}"})
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
