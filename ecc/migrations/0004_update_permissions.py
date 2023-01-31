6a15036e107831f5fa35f36a0f1d45c9fb7f522d
Mindiell <github@mindiell.net>
2023-01-31 18:44:38 +0100

    fix: mise à jour des permissions avant leur ajout

--- ecc/migrations/0004_update_permissions.py
+++ ecc/migrations/0004_update_permissions.py
@@ -1,5 +1,6 @@
 from django.db import migrations
 from django.contrib.auth.models import Permission, Group
+from django.contrib.auth.management import create_permissions
 from django.conf import settings
 
 PERMISSIONS = [
@@ -12,6 +13,11 @@ PERMISSIONS = [
 ]
 
 def update_permissions(apps, schema_editor):
+    for app_config in apps.get_app_configs():
+        app_config.models_module = True
+        create_permissions(app_config, verbosity=0)
+        app_config.models_module = None
+
     group = Group.objects.get_or_create(name='admin_metier')[0]
     for p in PERMISSIONS:
         permission = Permission.objects.get(codename=p)
